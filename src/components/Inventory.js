ff347601955ee3a13669594e175e2c4d8dd2cf4f
Steven19950530 <stevenlai19950530@gmail.com>
2017-09-30 15:29:13 -0300

    push forgotten files

---
+++ src/components/Inventory.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import AddFishForm from './AddFishForm'
+
+class Inventory  extends React.Component {
+    constructor() {
+        super()
+        this.handleChange = this.handleChange.bind(this)
+        this.renderInventory = this.renderInventory.bind(this)
+    }
+
+    handleChange(e, key) {
+        const fish = this.props.fishes[key]
+        const updatedFish = {...fish, [e.target.name]: e.target.value}
+        this.props.updateFish(key, updatedFish)
+    }
+
+    renderInventory(key) {
+        const fish = this.props.fishes[key]
+        return (
+            <div className="fish-edit" key={key}>
+                <input type="text" name="name" defaultValue={fish.name} placeholder="Fish name" 
+                    onChange={(e) => this.handleChange(e, key)} />
+                <input type="text" name="price" defaultValue={fish.price} placeholder="Fish price" 
+                    onChange={(e) => this.handleChange(e, key)} />
+                <select type="text" name="status" defaultValue={fish.status} placeholder="Fish status" 
+                    onChange={(e) => this.handleChange(e, key)} >
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea type="text" name="desc" defaultValue={fish.desc} placeholder="Fish desc" 
+                    onChange={(e) => this.handleChange(e, key)} ></textarea>
+                <input type="text" name="image" defaultValue={fish.image} placeholder="Fish image" 
+                    onChange={(e) => this.handleChange(e, key)} />
+            </div>
+        )
+    }
+    
+
+    render() {
+        return (
+            <div>
+                <h2>Inventory</h2>
+                {Object.keys(this.props.fishes).map(this.renderInventory)}
+                <AddFishForm addFish={this.props.addFish} />
+                <button onClick={this.props.loadSamples}>Load Samples fishes</button>
+            </div>
+            
+        )
+    }
+    
+}
+
+export default Inventory;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import { getFunName } from '../helpers';
+import '../css/style.css';
+
+class StorePicker extends React.Component {
+    // constructor() {
+    //     super();
+    //     this.goToStore = this.goToStore.bind(this)
+    // }
+    goToStore(e)  {
+        e.preventDefault();
+        const storeId = this.storeInput.value
+        this.context.router.transitionTo(`/store/${storeId}`)
+    }
+
+    render() {
+        return (
+            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+                <h2>Please enter a store</h2>
+
+                <input type="text" required placeholder="Store name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
+                <button type="submit">Enter -></button>
+            </form>
+        )
+    }
+}
+
+StorePicker.contextTypes = {
+    router: React.PropTypes.object
+}
+
+export default StorePicker;
