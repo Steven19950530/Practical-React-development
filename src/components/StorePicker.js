89313a039e3e68c4d7a699383ab2630030f67707
Steven19950530 <stevenlai19950530@gmail.com>
2017-10-01 18:19:12 -0300

    Show different stores to interact

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,32 @@
 import React from 'react';
-
+import base from '../base'
 import { getFunName } from '../helpers';
 import '../css/style.css';
 
 class StorePicker extends React.Component {
+    constructor() {
+        super()
+        this.renderTitles = this.renderTitles.bind(this)
+        this.state = {
+            titles: ''
+        }
+    }
+
+    renderTitles(key) {
+        return (
+            <code key={key}>{key}<br/></code>
+        )
+    }
+
+    componentWillMount() {
+        //TODO: Model differently the database
+        this.ref = base.fetch('/', {
+            context: this,
+            then(titles) {
+                this.setState({titles})
+            }
+        })
+    }
 
     goToStore(e)  {
         e.preventDefault();
@@ -13,12 +36,20 @@ class StorePicker extends React.Component {
 
     render() {
         return (
-            <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
-                <h2>Please enter a store</h2>
-
-                <input type="text" required placeholder="Store name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
-                <button type="submit">Enter -></button>
-            </form>
+            <div>
+                <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
+                    <h2>Please enter a store</h2>
+                    <input type="text" required placeholder="Store name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input }} />
+                    <button type="submit">Enter -></button>
+                </form>
+                <br/>
+                <form className="store-selector">
+                    <h2 className="store-selector">Existing store</h2>
+                    <p>Just copy this text and paste it above!</p>
+                    {Object.keys(this.state.titles).map(this.renderTitles)}
+                </form>
+            </div>
+            
         )
     }
 }
