a36bd42298d16859ff6cd4a3f53a1ce4db417df8
Steven19950530 <stevenlai19950530@gmail.com>
2017-10-01 19:38:04 -0300

    BETA: Changes to deploy on Bluemix

--- index.js
+++ index.js
@@ -1,38 +1,15 @@
 /**
  * Main page for Bluemix development
  */
-
-import compression from 'compression';
-import express from 'express';
-import fs from 'fs';
-import request from 'request';
- 
+const express = require('express')
+const port = process.env.PORT || 3001;
 const app = express();
-const port = process.env.PORT || 8080;
-app.use(compression());
 
-let keys;
-if (fs.existsSync('./keys.json')) {
-  keys = require('./keys.json');
-} else {
-  keys = JSON.parse(process.env.VCAP_SERVICES)['user-provided'][0].credentials;
-}
+app.use(express.static(`${__dirname}/ui-react/build`));
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/ui-react/build/index.html`);
+}) 
 
-app.get('/github', (req, res) => {
-  request({
-    url: `https://api.github.com/user/repos?affiliation=owner,collaborator&access_token=${keys.github}`,
-    headers: {
-      'user-agent': 'node.js',
-    },
-  }, (err, response, body) => {
-    if (!err && response.statusCode === 200) {
-      res.send(body);
-    }
-  });
-});
- 
-app.use(express.static('./build'));
- 
 app.listen(port, (err) => {
  if (err) {
    console.log(err);
