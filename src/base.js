1f0cf3cbbf230a0ca5bbf7211f13ce3dd5b227ab
Facundo Gordillo <facundogordillo@FMGordillo-MacBookPro.local>
2017-10-06 02:26:01 -0300

    Reorder (again)

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: 'AIzaSyDq_US4wKhmboS7kzsmHwvcbQ60A0_VZzs',
+  authDomain: 'testing-react-d998a.firebaseapp.com',
+  databaseURL: 'https://testing-react-d998a.firebaseio.com'
+});
+
+export default base;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import base from '../base';
+import { getFunName } from '../helpers';
+import '../css/style.css';
+
+class StorePicker extends React.Component {
+  constructor() {
+    super();
+    this.renderTitles = this.renderTitles.bind(this);
+    this.state = {
+      titles: ''
+    };
+  }
+
+  renderTitles(key) {
+    return (
+      <code key={key}>
+        {key}
+        <br />
+      </code>
+    );
+  }
+
+  componentWillMount() {
+    //TODO: Model differently the database
+    this.ref = base.fetch('/', {
+      context: this,
+      then(titles) {
+        this.setState({ titles });
+      }
+    });
+  }
+
+  goToStore(e) {
+    e.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <form className="store-selector" onSubmit={e => this.goToStore(e)}>
+          <h2>Please enter a store</h2>
+          <input
+            type="text"
+            required
+            placeholder="Store name"
+            defaultValue={getFunName()}
+            ref={input => {
+              this.storeInput = input;
+            }}
+          />
+          <button type="submit">Enter -></button>
+        </form>
+        <br />
+        <form className="store-selector">
+          <h2>Existing store</h2>
+          <p>Just copy one of these names and paste it above!</p>
+          {Object.keys(this.state.titles).map(this.renderTitles)}
+        </form>
+      </div>
+    );
+  }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
+};
+
+export default StorePicker;
